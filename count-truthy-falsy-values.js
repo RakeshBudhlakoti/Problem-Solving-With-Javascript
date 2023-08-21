7d67355f882ea476cd1071a97ecb4880daeab777
rakeshbudhlakoti <rakeshbudhlakoti1991@gmail.com>
2023-08-21 15:42:21 +0530

    New problem solving programs

---
+++ count-truthy-falsy-values.js
@@ -0,0 +1,15 @@
+const valuesArray = [0, 1, '', undefined, false, true];
+
+function checkCountTruthyFalsy(curArray) {
+  let truthyCount = 0;
+  
+  for (let value of curArray) {
+    // no need to check if(value !== false || value !== 0 || value !== '' or ...)
+    if (value) {
+      truthyCount++;
+    }
+  }
+  return truthyCount;
+}
+
+console.log(checkCountTruthyFalsy(valuesArray));
